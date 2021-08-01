2bef93e3f519665168f5b58c04aa05f98926c944
willwang0903 <406860686@qq.com>
2021-08-01 23:25:10 +0800

    ListTest

---
+++ ListTest.java
@@ -0,0 +1,84 @@
+import java.util.*;
+public class ListTest{
+    public List<Student> students;
+    public ListTest(){
+        this.students = new ArrayList<Student>();
+
+    }
+    public void testAdd(){
+        Student st1 = new Student("1","张三");
+        students.add(st1);
+        Student temp = students.get(0);
+        System.out.println("添加了学生："+temp.id+":"+temp.name);
+
+        Student st2 = new Student("2","李四");
+        students.add(0,st2);
+        Student temp2 = students.get(0);
+        System.out.println("添加了学生："+temp2.id+":"+temp2.name);
+
+        Student[] student = {new Student("3","王五"),new Student("4","马六")};
+        students.addAll(Arrays.asList(student));
+        Student temp3 = students.get(2);
+        Student temp4 = students.get(3);
+        System.out.println("添加了学生："+temp3.id+":"+temp3.name);
+        System.out.println("添加了学生："+temp4.id+":"+temp4.name);
+
+        Student[] student2 = {new Student("5","周七"),new Student("6","赵八")};
+        students.addAll(2,Arrays.asList(student2));
+        Student temp5 = students.get(2);
+        Student temp6 = students.get(3);
+        System.out.println("添加了学生："+temp5.id+":"+temp5.name);
+        System.out.println("添加了学生："+temp6.id+":"+temp6.name);
+
+    }
+    public void testGet(){
+        int size = students.size();
+        for(int i=0;i<size;i++){
+            Student st =  students.get(i);
+            System.out.println("学生："+st.id+":"+st.name);
+        }
+    }
+
+    //通过迭代器来遍历
+    public void testIterator(){
+        Iterator<Student> it = students.iterator();
+        System.out.println("有如下学生(通过迭代器访问)：");
+        while (it.hasNext()){
+            Student st = it.next();
+            System.out.println("学生"+st.id+":"+st.name);
+        }
+    }
+
+    //通过for each方法访问集合元素
+    public void testForEach(){
+        System.out.println("有如下学生(通过for each)：");
+        for(Student obj : students){
+            Student st = obj;
+            System.out.println("学生"+st.id+":"+st.name);
+        }
+        students.stream().sorted(Comparator.comparing(x -> x.id)).forEach(System.out::println);
+    }
+
+    //修改List中的元素
+    public void testModify(){
+        students.set(4,new Student("3","吴酒"));
+    }
+
+    //删除List中的元素
+    public void testRemove(){
+        Student st = students.get(4);
+        System.out.println("我是学生："+st.id+":"+st.name+",我即将被删除");
+        students.remove(st);
+        System.out.println("成功删除学生！");
+        testForEach();
+    }
+    public static void main(String[] args){
+        ListTest lt = new ListTest();
+        lt.testAdd();
+        lt.testGet();
+        lt.testIterator();
+        lt.testModify();
+        lt.testForEach();
+        lt.testRemove();
+    }   
+}
